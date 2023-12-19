3a8dbbb1df0d89edce536faa06cb7ae58baf1367
Eduardo <134222436+EduardoSymph@users.noreply.github.com>
2023-12-19 12:16:56 -0300

    primeiras-analises-sql

---
+++ analises_gerais.sql
@@ -0,0 +1,44 @@
+USE Techchannel;
+
+-- Soma de Valores por Responsáveis e Qtd total de Projetos por responsáveis --
+SELECT
+    Responsavel
+    ,COUNT(*) AS TotalProjetos
+    ,SUM(Proposta_Inicial) AS TotalPropostas
+FROM
+    vendas
+GROUP BY
+    Responsavel;
+
+-- Soma de Valores por Fabricante e Qtd total de Projetos por fabricante --
+SELECT
+    Fabricante
+    ,COUNT(*) AS TotalProjetos
+    ,SUM(Proposta_Inicial) AS TotalPropostas
+FROM
+    vendas
+GROUP BY
+    Fabricante;
+
+
+-- Soma de Valores por Fase e Qtd total de Projetos por fase --
+SELECT
+    Fase
+    ,COUNT(*) AS TotalProjetos
+    ,SUM(Proposta_Inicial) AS TotalPropostas
+FROM
+    vendas
+GROUP BY
+    Fase;
+
+
+-- Soma de valores por Quarter --
+SELECT
+    ((MONTH(Datas) - 1) / 3) + 1 AS Trimestre
+    ,SUM(Proposta_Inicial) AS SomaPropostaInicial
+FROM
+    vendas
+GROUP BY
+    ((MONTH(Datas) - 1) / 3) + 1
+ORDER BY
+    Trimestre;
